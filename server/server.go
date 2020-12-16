9ed25a8a4c6d90a2ed6a9b9acde45f635f2d5fbe
Julian Beck <ju-fa-beck@t-online.de>
2020-12-16 11:20:35 +0100

    added cors support

--- server/server.go
+++ server/server.go
@@ -2,10 +2,9 @@ package server
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"path/filepath"
-
-	"github.com/gin-gonic/gin"
 )
 
 func upload(c *gin.Context) {
@@ -25,12 +24,25 @@ func upload(c *gin.Context) {
 
 	c.String(http.StatusOK, fmt.Sprintf("File %s uploaded successfully ", file.Filename))
 }
-
+func CORS() gin.HandlerFunc {
+	// TO allow CORS
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
 func setupRouter() *gin.Engine {
 
 	// Disable Console Color
 	// gin.DisableConsoleColor()
 	router := gin.Default()
+	router.Use(CORS())
 	router.LoadHTMLGlob("server/template/*")
 
 	router.GET("/", func(c *gin.Context) {
