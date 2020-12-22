f86ded2684f05ddd85d1b9b996130aa92a04b171
Julian Beck <ju-fa-beck@t-online.de>
2020-12-22 16:45:22 +0100

    form updates

--- client/src/components/ui/FileList.js
+++ client/src/components/ui/FileList.js
@@ -1,18 +1,35 @@
 import 'react-dropzone-uploader/dist/styles.css'
 import Dropzone from 'react-dropzone-uploader'
+import { useState } from 'react'
 
-const Create = ({uploaded, fileData}) => {
+const FileUploader = ({ uploaded, fileData }) => {
+  const [id, setID] = useState(0)
+
   // specify upload params and url for your files
-  const getUploadParams = ({ meta }) => { return { url: 'http://localhost:8080/api/upload', method: 'POST', } }
-  
+  const getUploadParams = ({ meta }) => {
+    console.log('upload')
+    const idReturn = '/' + id
+    return {
+      url: `http://localhost:8080/api/upload${id !== 0 ? idReturn : ''}`,
+      method: 'POST'
+    }
+  }
+
   // called every time a file's `status` changes
-  const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
-  
+  const handleChangeStatus = ({ meta, file, xhr }, status) => {
+    console.log('status change')
+    console.log(status, meta, file)
+    if (status === 'done') {
+      var json = JSON.parse(xhr.response)
+      setID(json.id)
+    }
+  }
+
   // receives array of files that are done uploading when submit button is clicked
   const handleSubmit = (files, allFiles) => {
-    console.log(files);
-    console.log(allFiles);
-    uploaded(true);
+    console.log(files)
+    console.log(allFiles)
+    uploaded(true)
     fileData(allFiles)
   }
 
@@ -21,8 +38,11 @@ const Create = ({uploaded, fileData}) => {
       getUploadParams={getUploadParams}
       onChangeStatus={handleChangeStatus}
       onSubmit={handleSubmit}
-      accept="image/*,audio/*,video/*"
+      accept='image/*'
+      canCancel={false}
+      canRemove={false}
+      canRestart={false}
     />
   )
 }
-export default Create
+export default FileUploader
--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,7 +1,10 @@
 import { Container, Heading } from '@chakra-ui/react'
 import FileList from '../components/ui/FileList'
 import ScreenshotPreview from "../components/ui/ScreenshotPreview"
+import Form from "../components/ui/Form"
+
 import React, { useState } from 'react'
+
 
 const Create = () => {
   const [isUploaded, setIsUploaded] = useState(false)
@@ -19,12 +22,14 @@ const Create = () => {
       >
         Upload Your Screnshots 
       </Heading>
+      <Form/>
       <p>Files Uploaded {isUploaded.toString()}</p>
       <p>Files Data {Object.keys(fileData).length}</p>
       {isUploaded ? <ScreenshotPreview url={fileData[0].meta.previewUrl}/> : (
         <FileList
           uploaded={isUploaded => setIsUploaded(isUploaded)}
           fileData={fileData => setFileData(fileData)}
+          canRemove={false}
         />
       )}
     </Container>
