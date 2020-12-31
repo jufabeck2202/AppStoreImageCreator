c4633a59ba8cd2dfbf43e05af4e34720b89d32ab
Julian Beck <ju-fa-beck@t-online.de>
2020-12-31 14:11:46 +0100

    colorpicker popup

--- client/src/components/ui/Picker.js
+++ client/src/components/ui/Picker.js
@@ -1,12 +1,35 @@
+import { HexColorPicker } from 'react-colorful'
+import { useState } from 'react'
+import {
+  Popover,
+  Button,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverBody,
+  PopoverHeader,
+  PopoverCloseButton
+} from '@chakra-ui/react'
 
-import { HexColorPicker } from "react-colorful";
-import { useState} from 'react'
-
-import "react-colorful/dist/index.css";
+import 'react-colorful/dist/index.css'
 
 const Picker = () => {
-  const [color, setColor] = useState("#aabbcc");
-  return <HexColorPicker color={color} onChange={setColor} />;
-};
+  const [color, setColor] = useState('#aabbcc')
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Button>Select Background Color</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverArrow />
+        <PopoverCloseButton />
+        <PopoverHeader>Select Background Color!</PopoverHeader>
+        <PopoverBody>
+          <HexColorPicker color={color} onChange={setColor} />
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
+  )
+}
 
-export default Picker;
+export default Picker
